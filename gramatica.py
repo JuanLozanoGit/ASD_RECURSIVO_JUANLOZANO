71f0b14f3dfe1b8980b8dee53bcda35a791f390f
JuanLozanoGit <juancamilolozanocortesunal@gmail.com>
2026-04-13 15:21:36 -0500

    Update gramatica.py

--- gramatica.py
+++ gramatica.py
@@ -1,18 +1,21 @@
-# Definicion de terminales y no terminales para los ejercicios
-# Usamos 'e' o 'epsilon' para la cadena vacia, una definicion aprendida en la clase del miercoles.
+# Definicion de gramaticas y simbolos
+# 'e' representa epsilon (vacio)
 
-EJERCICIO_1 = {
-    'S': [['A', 'uno', 'B', 'C'], ['S', 'dos']],
-    'A': [['B', 'C', 'D'], ['A', 'tres'], ['ε']],
-    'B': [['D', 'cuatro', 'C', 'tres'], ['ε']],
-    'C': [['cinco', 'D', 'B'], ['ε']],
-    'D': [['seis'], ['ε']]
-}
+TERMINALES = {'uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'id', 'igual', 'num', 'puntoycoma', '$'}
 
+# Gramatica del Ejercicio 2 (La del Ejercicio 1 tiene recursividad y rompe el LL1)
 EJERCICIO_2 = {
     'S': [['A', 'B', 'uno']],
-    'A': [['dos', 'B'], ['ε']],
-    'B': [['C', 'D'], ['tres'], ['ε']],
+    'A': [['dos', 'B'], ['e']],
+    'B': [['C', 'D'], ['tres'], ['e']],
     'C': [['cuatro', 'A', 'B'], ['cinco']],
-    'D': [['seis'], ['ε']]
+    'D': [['seis'], ['e']]
+}
+
+# Gramatica del Ejercicio 3 (Un IF o ASIGNACION simple)
+EJERCICIO_3 = {
+    'S': [['A', 'B']],
+    'A': [['id', 'igual', 'E']],
+    'B': [['puntoycoma'], ['e']],
+    'E': [['num'], ['id']]
 }
